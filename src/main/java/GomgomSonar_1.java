a5c2ab3e88270490ba4c1940054fc5534056db43
gomgom <gomgom@doku.com>
2018-10-24 15:11:18 +0700

    firstSonar

---
+++ src/main/java/GomgomSonar_1.java
@@ -0,0 +1,15 @@
+public class GomgomSonar_1 {
+    String pass="sfs";
+    String name="gom";
+
+    public String cek(String name){
+        if (name.equals("gomgom")){
+            System.out.println("sama");
+        }
+        else{
+            System.out.println("tidak");
+        }
+        return name;
+    }
+
+}
