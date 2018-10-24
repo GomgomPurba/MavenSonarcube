9387f15569ee1f8a37a5a9d5f965cc0e540248fc
gomgom <gomgom@doku.com>
2018-10-24 15:44:01 +0700

    thirdSonar git commit -m thirdSonar

--- src/main/java/GomgomSonar1.java
+++ src/main/java/GomgomSonar1.java
@@ -1,11 +1,13 @@
-public class GomgomSonar_1 {
+public class GomgomSonar1 {
     String pass="sfs";
     String name="gom";
 
     public String cek(String name){
         if (name.equals("gomgom")){
             System.out.println("sama");
-        } else {
+        }
+        else
+        {
             System.out.println("tidak");
         }
         return name;
